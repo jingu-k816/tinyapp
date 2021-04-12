03a0577d580bfe2a9490af22bdef40820da38c2e
Jingu Kang <jinguk0816@gmail.com>
2021-04-12 18:13:41 -0400

    added a funtion that generates random 6 characters including numbers. Also added a post method on /urls path

--- express_server.js
+++ express_server.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
+
+const generateRandomString = () => {
+  let randomString = '';
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (let i = 0; i <= 5; i++) {
+    let randomIndex = Math.floor(Math.random() * chars.length);
+    let randomValue = chars[randomIndex];
+    randomString += randomValue;
+  }
+  return randomString;
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -24,6 +38,15 @@ app.get('/hello', (req, res) => {
 app.get('/urls', (req, res) => {
   const templateVars = {urls: urlDatabase};
   res.render('urls_index', templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get('/urls/:shortURL', (req, res) => {
