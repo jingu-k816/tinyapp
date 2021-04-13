e9f69d550692da5a5119b80eb88bfbc0ea555a6c
Jingu Kang <jinguk0816@gmail.com>
2021-04-12 20:55:54 -0400

    added a get method on shortURL page when the user clicks on the shortURL code on the url/shortCode page, it redirects to the matching URL

--- express_server.js
+++ express_server.js
@@ -1,14 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 8080;
 const bodyParser = require("body-parser");
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 
 const generateRandomString = () => {
-  let randomString = '';
-  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let randomString = "";
+  const chars =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
   for (let i = 0; i <= 5; i++) {
     let randomIndex = Math.floor(Math.random() * chars.length);
@@ -16,42 +17,51 @@ const generateRandomString = () => {
     randomString += randomValue;
   }
   return randomString;
-}
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
 };
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send("Hello");
 });
 
-app.get('/urls.json', (req, res) => {
+app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get('/hello', (req, res) => {
-  res.send('<html><body>Hello <b>World</b></body></html>\n');
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.get('/urls', (req, res) => {
-  const templateVars = {urls: urlDatabase};
-  res.render('urls_index', templateVars);
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  const randomShortURL = generateRandomString();
+  urlDatabase[randomShortURL] = req.body.longURL;
+  res.redirect(`/urls/${randomShortURL}`);
 });
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-app.get('/urls/:shortURL', (req, res) => {
-  const templateVars = {shortURL : req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
-  res.render('urls_show', templateVars);
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+  };
+  res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
 app.listen(PORT, () => {
