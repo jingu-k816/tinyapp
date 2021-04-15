9751d8f83ff29bd84c60997301606adf9b849418
Jingu Kang <jinguk0816@gmail.com>
2021-04-15 14:57:05 -0400

    moved helper functions to helpers.js

---
+++ helpers.js
@@ -0,0 +1,44 @@
+/*********************************************************** */
+//Helper Functions
+
+const emailCheck = (email, users) => {
+  for (const key in users) {
+    if (users[key].email === email) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const generateRandomString = () => {
+  let randomString = "";
+  const chars =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (let i = 0; i <= 5; i++) {
+    let randomIndex = Math.floor(Math.random() * chars.length);
+    let randomValue = chars[randomIndex];
+    randomString += randomValue;
+  }
+  return randomString;
+};
+
+const lookUpUser = (email, password, users) => {
+  for (const key in users) {
+    if (users[key].email === email && bcrypt.compareSync(password, users[key].password)) {
+      return key;
+    }
+  }
+  return false;
+};
+
+const urlsForUser = (id, urlDatabase) => {
+  const urls = {};
+  for (const key in urlDatabase) {
+    if (urlDatabase[key].userID === id) {
+      urls[key] = urlDatabase[key];
+    }
+  }
+  return urls;
+};
+module.exports = {emailCheck, urlsForUser, generateRandomString, lookUpUser};
